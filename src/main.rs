599565e353b20f84d050091780c2d4fd6f9f5a27
ahammer0 <axel.schwindenhammer@gmail.com>
2025-03-06 01:34:35 +0100

    parenthesis ok + tests

--- src/main.rs
+++ src/main.rs
@@ -95,11 +95,11 @@ struct Expression {
     op: OpKind,
 }
 
-// TODO: add precedence for mult & div
 fn parser(mut lx: Vec<Token>) -> ExpOrValue {
     println!("appel de parser avec lx: {:?}", &lx);
     //handle end of lexer
     if lx.len() == 1 {
+        //unwrap should work since we checked that lx contains one value
         match lx.pop().unwrap() {
             Token::Value(v) => {
                 return ExpOrValue::Val(v);
@@ -107,65 +107,132 @@ fn parser(mut lx: Vec<Token>) -> ExpOrValue {
             _ => panic!("lexex with only one item should contain a value"),
         }
     }
-
-    //searching for plus or minus operators
-    println!("lx avant de chercher +-: {:?}", &lx);
-    let pm_pos = lx
-        .iter()
-        .position(|k| *k == Token::Op(OpKind::Add) || *k == Token::Op(OpKind::Sub));
-
-    println!("lx apres de chercher +-: {:?}", &lx);
-    println!("pm_pos:{:?}", &pm_pos);
+    // Hanldling all cases:
+    // Search for +- since they are less priority operators
+    let mut lx_iter = lx.iter();
+    let mut next_token = lx_iter.next();
+    let mut left = Vec::new();
     // if found +- operators then compute left&right vectors
-    if let Some(pos) = pm_pos {
-        let right = lx.split_off(pos + 1);
-        println!("right apres split: {:?}", &right);
-
-        if let Token::Op(op) = lx.get(pos).unwrap().clone() {
-            lx.pop();
-            let left = lx;
-            println!("left apres split et pop: {:?}", &left);
-
-            ExpOrValue::Exp(Box::new(Expression {
-                left: (parser(left)),
-                right: (parser(right)),
-                op,
-            }))
-        } else {
-            //this could not be before of previous serach
-            panic!("should be operators, opKind should be Add or Sub")
-        }
-    } else {
-        // +- not found, processing */ operators
-        //
-        dbg!(&lx);
-        let md_pos = lx
-            .iter()
-            .position(|k| *k == Token::Op(OpKind::Mul) || *k == Token::Op(OpKind::Div));
-
-        println!("md_pos:{:?}", &md_pos);
-        // if found */ operators then compute left&right vectors
-        if let Some(pos) = md_pos {
-            let right = lx.split_off(pos + 1);
-
-            if let Token::Op(op) = lx.get(pos).unwrap().clone() {
-                lx.pop();
-                let left = lx;
-
-                ExpOrValue::Exp(Box::new(Expression {
-                    left: (parser(left)),
-                    right: (parser(right)),
-                    op,
-                }))
-            } else {
-                //this could not be before of previous serach
-                panic!("should be operators, opKind should be Div or Mul")
+    let mut par_count: usize = 0;
+    while let Some(token) = next_token {
+        // iter through all tokens until we find + or - not in parenthesis and store the left vector
+        match token {
+            Token::Op(op) => {
+                // we have found a +- operator, we store the left vector and continue
+                match op {
+                    OpKind::Add | OpKind::Sub => {
+                        // when found, we store the operand and the remaining items in a vector
+                        if par_count == 0 {
+                            let right: Vec<Token> = lx_iter.map(|x| x.to_owned()).collect();
+                            //     return Expression
+                            return ExpOrValue::Exp(Box::new(Expression {
+                                left: (parser(left)),
+                                right: (parser(right)),
+                                op: *op,
+                            }));
+                        }
+                        left.push(token.clone());
+                        next_token = lx_iter.next();
+                    }
+                    _ => {
+                        left.push(token.clone());
+                        next_token = lx_iter.next();
+                    }
+                }
             }
-        } else {
-            // +- not found, normal procesing
-            panic!("expression should contain at least one operator");
+            Token::Delimiter(DKind::Open) => {
+                par_count += 1;
+                left.push(token.clone());
+                next_token = lx_iter.next();
+            }
+            Token::Delimiter(DKind::Close) => {
+                par_count -= 1;
+                left.push(token.clone());
+                next_token = lx_iter.next();
+            }
+            _ => {
+                left.push(token.clone());
+                next_token = lx_iter.next();
+            }
+        }
+        // TODO: if iterator not emptied, returning the Expression with the left, right vector and the operand
+        //
+    }
+    // At this point we should have no more +- at base level
+    //
+    let old_left = left;
+    lx_iter = old_left.iter();
+    next_token = lx_iter.next();
+    left = Vec::new();
+
+    // Iter through previous left vector until emptied
+    if let Some(token) = next_token {
+        match token {
+            Token::Value(val) => {
+                //if value, expect operand then set right to the remaining tokens
+                //    return Expression
+                next_token = lx_iter.next();
+                let right: Vec<Token> = lx_iter.map(|x| x.to_owned()).collect();
+                if let Some(Token::Op(op)) = next_token {
+                    return ExpOrValue::Exp(Box::new(Expression {
+                        left: (ExpOrValue::Val(*val)),
+                        right: (parser(right)),
+                        op: *op,
+                    }));
+                } else if next_token.is_none() {
+                    return ExpOrValue::Val(*val);
+                } else {
+                    panic!("token after value should be operand");
+                }
+            }
+            Token::Delimiter(DKind::Open) => {
+                //if parenthesis, store content until matching,
+                let mut par_count: usize = 1;
+                next_token = lx_iter.next();
+                while par_count != 0 {
+                    if let Some(token) = next_token {
+                        match token {
+                            Token::Delimiter(DKind::Open) => {
+                                par_count += 1;
+                            }
+                            Token::Delimiter(DKind::Close) => {
+                                par_count -= 1;
+                            }
+                            _ => {}
+                        }
+                        left.push(token.clone());
+                        next_token = lx_iter.next();
+                    } else {
+                        panic!("unmatching parenthesis")
+                    }
+                }
+                //strip off last parenthesis
+                left.pop();
+
+                //check next value for operand
+                if let Some(token) = next_token {
+                    match token {
+                        //if operand, store it and set right to remaining items and return Expression
+                        Token::Op(op) => {
+                            let right: Vec<Token> = lx_iter.map(|x| x.to_owned()).collect();
+                            return ExpOrValue::Exp(Box::new(Expression {
+                                left: (parser(left)),
+                                right: (parser(right)),
+                                op: *op,
+                            }));
+                        }
+                        _ => panic!("should be operand after parenthesis"),
+                    }
+                } else {
+                    return parser(left);
+                }
+            }
+            _ => panic!("should be an operand"),
         }
     }
+
+    parser(left)
+    //              else, iterator is emptied, return parser(stripped from parenthesis)
 }
 
 fn compute(ex: ExpOrValue) -> usize {
@@ -192,6 +259,7 @@ fn compute(ex: ExpOrValue) -> usize {
 fn compute_string(st: &str) -> usize {
     let lx = to_lexer(st.to_string());
     let exp = parser(lx);
+    dbg!(&exp);
     compute(exp)
 }
 fn main() {
@@ -229,5 +297,25 @@ mod tests {
     fn parenthesis_prio() {
         let result = compute_string("(1+2)*3+4");
         assert_eq!(result, 13);
+    }
+    #[test]
+    fn parenthesis_prio_rev() {
+        let result = compute_string("1*2+(3+4)");
+        assert_eq!(result, 9);
+    }
+    #[test]
+    fn par_unwrap() {
+        let result = compute_string("(1+2)");
+        assert_eq!(result, 3);
+    }
+    #[test]
+    #[should_panic]
+    fn par_folowwed_by_value() {
+        compute_string("(1+2)5");
+    }
+    #[test]
+    #[should_panic]
+    fn par_unmatched() {
+        compute_string("(1+2");
     }
 }
